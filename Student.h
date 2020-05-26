82eafda4ce94d31f2848154b0706a01c8858287a
ArgChris5719 <argyr@ceid.upatras.gr>
2020-05-26 08:25:24 +0300

    Student class header file

---
+++ Student.h
@@ -0,0 +1,45 @@
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
+#include<vector>
+
+using namesace std;
+
+class Alert;
+class Room;
+class Group;
+class Post;
+
+class Student: public User{
+private:
+    vector<Group*> belongs_in;
+    vector<Room*> follows;
+    vector<Post*> activity;
+public:
+    int AM;
+    int overall_score;
+    int yearly_score;
+    int semester_score;
+
+    int overall_rank;
+    int yearly_rank;
+    int semester_rank;
+
+    Student();
+    Student(User, int);
+    ~Student();
+
+    void Create_Group();
+    void Save_Group();
+    Student SearchStudents();
+    void EditGroupRole();
+    void StudentsAccept();
+    void StudentsDecline();
+    void ShowMsgofAcceptance();
+    void ErrorMsg();
+    void CreateOwnTimetable();
+    void EditAlerts();
+    void SaveChanges();
+    void editinfo();
+
+
+};
