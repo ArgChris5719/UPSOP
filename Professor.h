fe63db3a2516cd03ed958fd2f9fd3659272f5caf
ArgChris5719 <argyr@ceid.upatras.gr>
2020-05-26 11:37:23 +0300

    Professor class header

---
+++ Professor.h
@@ -0,0 +1,26 @@
+#ifndef Professor_H_INCLUDED
+#define STUDENT_H_INCLUDED
+#include<vector>
+
+using namesace std;
+
+
+class Room;
+class Class;
+class GradeList;
+
+class Professor: public User{
+
+private:
+    vector<Room*> in_chargeOf;
+    vector<Class*> class_list;
+    vector<GradeList*> grades;
+
+public:
+    void confirm_as_prof();
+    GradeList& grade();
+    void show_classes();
+    void choose_class();
+
+};
+
