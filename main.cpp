f23e507370dec608327eddeb280b20b53577e40a
avoritis <65782786+avoritis@users.noreply.github.com>
2020-05-30 01:10:23 +0300

    Add files via upload
    User.h : Added vector that holds all users and updates everytime a new one is created
                 completed initialisation of User, Student and Professor Class
    User.cpp : added Users' vector inside User constructor
    Room.cpp : declared some of the methods necessary to compile Professor.cpp
    main.cpp : added an example of registered users (professors, students)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "User.h"
+#include "User.cpp"
+
+
+vector<User> user_db;
 using namespace std;
 
-int main(){
+int main()
+{
+    Student st1("avoritis", "avoritis@ceid.upatras.gr");
+    Student st2("argiropou", "argiropou@ceid.upatras.gr", 5719);
+    Student st3;
 
+    //Student st2("ckaralis", "ckaralis@ceid.upatras.gr");
 
+    Professor pf1("vergos", "h_vergos@yahoo.gr");
+    Professor pf2;
+
+    cout << "Number of Users: " << user_db.size() << endl;
+    //user_db.clear();
+    return 0;
 }
